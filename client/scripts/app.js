03164b2f4f11d61eb796e8071b4dce7d66f9b222
Hack Reactor Student <student@hackreactor.com>
2017-05-26 14:42:07 -0700

    passes init and sending tests

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,23 @@
 // YOUR CODE HERE:
+var app = {};
+
+app.init = function() {
+  return true;
+};
+
+app.send = function(messageObj) {
+  $.ajax({
+    // This is the url you should use to communicate with the parse API server.
+    url: 'http://parse.CAMPUS.hackreactor.com/chatterbox/classes/messages',
+    type: 'POST',
+    data: JSON.stringify(messageObj),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent');
+    },
+    error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message', data);
+    }
+  });
+};
