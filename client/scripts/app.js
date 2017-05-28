398be31cc54c33c6033ea1acfa2c5e3e8c811a85
Hack Reactor Student <student@hackreactor.com>
2017-05-27 17:41:43 -0700

    passes chatroom behavior and events tests and adds friend section to the html index

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,8 +1,10 @@
 var app = {};
-app.server = 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages/';
+app.server = 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages';
 
 app.init = function() {
   app.fetch();
+  app.handleSubmit();
+  app.handleUsernameClick();
   return true;
 };
 
@@ -28,17 +30,20 @@ app.fetch = function() {
     // This is the url you should use to communicate with the parse API server.
     url: app.server,
     type: 'GET',
+    data: {limit: 20, order: '-createdAt'},
     contentType: 'application/json',
     success: function (data) {
       console.log('chatterbox: Message received', data);
       var roomArr = [];
-      for (var i = 0; i < data.results.length; i++) {
-        app.renderMessage(data.results[i]);
-        if (!roomArr.includes(data.results[i].roomname)) {
-          roomArr.push(data.results[i].roomname);
-          app.renderRoom(data.results[i].roomname);
-        } 
-      }
+      $.each(data['results'], function (objectIndex, objectVal) {
+        app.renderMessage(objectVal);
+        if (!roomArr.includes(objectVal.roomname)) {
+          roomArr.push(objectVal.roomname);
+          app.renderRoom(objectVal.roomname);
+        }
+      });
+      var users = [];
+      app.handleUsernameClick(users);
     },
     error: function (data) {
       // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
@@ -47,9 +52,37 @@ app.fetch = function() {
   });
 };
 
-app.filterMessages = function() {
+app.handleSubmit = function() {
+  $( '#send' ).submit(function( event ) {
+    event.preventDefault();
+    if ( $( '#message' ).val() !== '' ) {
+      var path = document.URL;
+      var pathArray = path.split('/');
+      var lastegment = pathArray[pathArray.length - 1];
+      var user = lastegment.substring(lastegment.lastIndexOf('=') + 1);
+      var messageObj = {
+        username: user,
+        text: $( '#message' ).val(),
+        roomname: $('#roomSelect').val()
+      };
+      console.log(messageObj);
+      app.send(messageObj);
+      app.fetch();
+    }
+  });
+};
+
+app.filterMessages = function(data) {
   //add an event listener to #roomSelect
-  //filter rooms based on room class
+  $('#roomSelect').change(function() {
+    //filter rooms based on room class
+      //remove messages
+      //app.clearMessags();
+      //make a fetch call
+    console.log(data);
+    $('#chats').find('".' + data.roomname + '"');
+  });
+  
 }; 
 
 
@@ -58,15 +91,14 @@ app.clearMessages = function() {
 };
 
 app.renderMessage = function(message) {
-  var messageDiv = $('<div class="message">');
-  var usernameTitle = $('<h3>');
+  var messageDiv = $('<div class="chat"' + 'id="' + message.username + '">');
+  var usernameTitle = $('<div class="username">');
   var textContent = $('<p>');
   usernameTitle.text(message.username);
   textContent.text(message.text);
   messageDiv.append(usernameTitle);
   messageDiv.append(textContent);
-  // add a room class to messageDiv
-  $('#chats').append(messageDiv);
+  $('#chats').prepend(messageDiv);
 };
 
 app.renderRoom = function(roomName) {
@@ -74,3 +106,15 @@ app.renderRoom = function(roomName) {
   room.text(roomName);
   $('#roomSelect').append(room);
 };
+
+app.handleUsernameClick = function(users) {
+  users = users || [];
+  $('.username').click(function(event) {
+    if (!users.includes(event.target.innerText)) {
+      users.push(event.target.innerText);
+      var friendname = `<div class="friend"> ${event.target.innerText} </div>`;
+      $('#friend').append(friendname);
+    }
+  });
+};
+
